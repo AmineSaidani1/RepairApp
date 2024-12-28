4dc16daa97fb68320344535ced409226f4a76c33
GrumblyDani1060 <grumblydani1060@gmail.com>
2024-12-28 17:09:03 +0000

    Creado prototipo de clase Service

--- src/org/ulpgc/is1/model/Service.java
+++ src/org/ulpgc/is1/model/Service.java
@@ -1,4 +1,50 @@
 package org.ulpgc.is1.model;
 
 public class Service {
+
+    private static int NEXT_ID = 0;
+    private final int id;
+
+    private ServiceType type;
+
+    private String description;
+
+    public static int getNextId() {
+        return NEXT_ID;
+    }
+
+    public static void setNextId(int nextId) {
+        NEXT_ID = nextId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public ServiceType getType() {
+        return type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setType(ServiceType type) {
+        this.type = type;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Service(int id, ServiceType type, String description) {
+        this.id = id;
+        this.type = type;
+        this.description = description;
+    }
+
+    public pay(){
+
+
+    }
 }
