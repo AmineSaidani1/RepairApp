e5decc2cfc70d9385ada45400ca7f3c9983efe26
amine <amin@exemple.com>
2024-12-29 12:49:01 +0100

    cambios en las clases Customer, Device, Employee y ServiceManager

--- src/org/ulpgc/is1/model/Device.java
+++ src/org/ulpgc/is1/model/Device.java
@@ -1,4 +1,22 @@
 package org.ulpgc.is1.model;
 
+import java.util.*;
+
 public class Device {
+    public String serialNumber;
+    public DeviceType deviceType;
+    public Customer owner;
+    public List<Service> deviceServices;
+
+    public Device(String serialNumber, DeviceType deviceType, Customer owner) {
+        this.serialNumber = serialNumber;
+        this.deviceType = deviceType;
+        this.owner = owner;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+
 }
--- src/org/ulpgc/is1/model/ServiceManager.java
+++ src/org/ulpgc/is1/model/ServiceManager.java
@@ -1,4 +1,86 @@
 package org.ulpgc.is1.model;
 
+import java.util.*;
+
 public class ServiceManager {
+    private List<Customer> customers;
+    private List<Device> devices;
+    private List<Employee> technicians;
+
+    public void addCustomer(Customer customer) {
+        if (customers == null) {
+            customers = new ArrayList<>();
+        }
+        if (!customers.contains(customer)) {
+            customers.add(customer);
+        }
+        else {
+            System.out.println("Customer already exists");
+        }
+    }
+    public Customer getCustomer(int id) {
+        for (int i = 0; i < customers.size(); i++) {
+            if (customers.get(i).getId() == id) {
+                return customers.get(i);
+            }
+        }
+        return null;
+    }
+
+    public void addDevice(Device device) {
+        if (devices == null) {
+            devices = new ArrayList<>();
+        }
+        if (!devices.contains(device)) {
+            devices.add(device);
+        }
+        else {
+            System.out.println("Device already exists");
+        }
+    }
+    public Device getDevice(String serialNumber) {
+        for (int i = 0; i < devices.size(); i++) {
+            if (devices.get(i).getSerialNumber().equals(serialNumber)) {
+                return devices.get(i);
+            }
+        }
+        return null;
+    }
+    public void addTechnician(Employee employee) {
+        if (technicians == null) {
+            technicians = new ArrayList<>();
+        }
+        if (!technicians.contains(employee)) {
+            technicians.add(employee);
+        }
+        else {
+            System.out.println("Employee already exists");
+        }
+    }
+    public Employee getTechnician(int number) {
+        for (int i = 0; i < technicians.size(); i++) {
+            if (technicians.get(i).getNumber() == number) {
+                return technicians.get(i);
+            }
+        }
+        return null;
+    }
+    public void service() {
+
+    }
+
+    public void payService() {
+
+    }
+
+    public List<Service> getDeviceServiceList(String serialNumber) {
+        List<Service> s = getDevice(serialNumber).deviceServices;
+        if (s == null) {
+            System.out.println("No device services found");
+            return null;
+        }
+        return s;
+    }
+
+
 }
